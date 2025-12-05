c3116d3de4d0ad4e89735e956f9d211ef92d1d82
David Duran <20960328+totallynotdavid@users.noreply.github.com>
2025-12-04 21:02:50 -0500

    fix: move &amp to the end to avoid double-unescaping

--- src/url.ts
+++ src/url.ts
@@ -9,11 +9,11 @@ export function normalizeUrls(rawUrls: string[]): string[] {
 
 function decodeHtmlEntities(text: string): string {
   return text
-    .replace(/&amp;/g, "&")
     .replace(/&lt;/g, "<")
     .replace(/&gt;/g, ">")
     .replace(/&quot;/g, '"')
-    .replace(/&#39;/g, "'");
+    .replace(/&#39;/g, "'")
+    .replace(/&amp;/g, "&");
 }
 
 function normalizeUrl(url: string): string {
